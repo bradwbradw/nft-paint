f9f9961f18863c87c0e3288da774498c07ca14cb
Brad <brad@secretstudy.ca>
2023-07-10 14:13:41 -0700

    panels tweak; keep transparency

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 
 const MyComponent = ({ left, right }) => {
   const [dimension, setDimension] = useState(50);
-  const isDragging = useRef(false);
+  const [isDragging, setIsDragging] = useState(false);
   const [isMobile, setIsMobile] = useState(
     window.innerWidth <= 768 && window.innerHeight <= 1024
   );
   const text = "Hello World ".repeat(1000);
 
   const handleMouseMove = (event) => {
-    if (isDragging.current) {
+    if (isDragging) {
       if (isMobile) {
         setDimension((event.touches[0].clientY / window.innerHeight) * 100);
       } else {
@@ -19,11 +19,11 @@ const MyComponent = ({ left, right }) => {
   };
 
   const handleMouseDown = () => {
-    isDragging.current = true;
+    setIsDragging(true);
   };
 
   const handleMouseUp = () => {
-    isDragging.current = false;
+    setIsDragging(false);
   };
 
   useEffect(() => {
@@ -44,7 +44,23 @@ const MyComponent = ({ left, right }) => {
         document.removeEventListener("mouseup", handleMouseUp);
       }
     };
-  }, [isMobile]);
+  }, [isMobile, isDragging]);
+
+  function DragMask() {
+    return (
+      isDragging && (
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        />
+      )
+    );
+  }
 
   return (
     <div
@@ -63,22 +79,15 @@ const MyComponent = ({ left, right }) => {
         }}
       >
         {left}
-        {isDragging.current && (
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-            }}
-          />
-        )}
+        <DragMask />
       </div>
+
       <div
         style={{
-          flexBasis: isMobile ? "5px" : "5px",
-          backgroundColor: "purple",
+          flexBasis: "6px",
+          backgroundColor: "rgba(0, 0, 0, 0.1)",
+          margin: "5px",
+          border: "3px dotted grey",
           cursor: isMobile ? "row-resize" : "col-resize",
         }}
         onMouseDown={handleMouseDown}
@@ -88,6 +97,7 @@ const MyComponent = ({ left, right }) => {
           }
         }}
       />
+
       <div
         style={{
           flexBasis: `${100 - dimension}%`,
